67258608118d731a1a8b74029f670d138c24571d
izelenev <the_lenev.mail.ru>
2017-02-12 15:40:24 +0300

    Add Bootstrap

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -4,9 +4,9 @@
 	<meta charset="utf-8">
 	<title>Прогноз погоды</title>
 	<link rel="icon" type="image/png" href="OWM.png" />
-	<link rel="stylesheet" href="/bootstrap/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
 	<script src="/js/jquery-1.12.4.min.js" type="text/javascript"></script>
-    <script type="text/javascript" src="/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="/bootstrap/js/bootstrap.min.js"></script>
 </head>
 <body>
 	<div class="container">
