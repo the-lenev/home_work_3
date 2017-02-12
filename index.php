1c825ce5ec4e12bd6901dbdebf6f6ab87bbb07cf
izelenev <the_lenev.mail.ru>
2017-02-12 15:11:48 +0300

    Create base (parent) class Controller, View, Model && Include them in index.php

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+require_once 'application/model.php';
+require_once 'application/view.php';
+require_once 'application/controller.php';
+require_once 'route.php';
+Route::start(); // запускаем маршрутизатор
