abd98c9223670f57513b4cc9cccd683367b875c9
izelenev <the_lenev.mail.ru>
2017-02-12 15:14:56 +0300

    Create base view for all pages

---
+++ application/view/template_view.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8">
+	<title>Прогноз погоды</title>
+	<link rel="icon" type="image/png" href="OWM.png" />
+	<link rel="stylesheet" href="/bootstrap/dist/css/bootstrap.min.css">
+	<script src="/js/jquery-1.12.4.min.js" type="text/javascript"></script>
+    <script type="text/javascript" src="/bootstrap/dist/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<?php include 'application/views/'.$content_view; ?>
+	</div>
+</body>
+</html>
